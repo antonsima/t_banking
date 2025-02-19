d642ba5199eb5c37410ec05373f371e23e7cb255
Anton <anton_sima@mail.ru>
2025-02-19 22:54:48 +0500

    Переписаны тесты

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from src.reports import spending_by_category
 from src.utils import get_data_frame_from_excel_file
 from src.views import get_cashback_categories, get_main_page
 
-DATE_TO_TEST = '2021-11-13 10:00:00'
+DATE_TO_TEST = '2022-03-30 22:22:03'
 PATH_TO_EXCEL = os.path.join(DATA_DIR, "operations.xlsx")
 TRANSACTIONS = get_data_frame_from_excel_file(PATH_TO_EXCEL)
 TRANSACTIONS_DICT = TRANSACTIONS.to_dict(orient='records')
@@ -18,4 +18,4 @@ print(get_main_page(DATE_TO_TEST, TRANSACTIONS))
 print(get_cashback_categories(TRANSACTIONS_DICT, 2021, 11))
 
 # Отчет
-print(spending_by_category(TRANSACTIONS, 'Супермаркеты', DATE_TO_TEST))
+print(spending_by_category(TRANSACTIONS, 'Переводы', DATE_TO_TEST))
--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,47 +1,31 @@
 import os
 
+import pandas as pd
 from freezegun import freeze_time
 
 from config import DATA_DIR
-from src.reports import get_report_func_result, spending_by_category
+from src.reports import spending_by_category
 from src.utils import get_data_frame_from_excel_file
 
-DATE_TO_TEST = '2021-11-13 10:00:00'
+DATE_TO_TEST = '2022-03-30 22:22:03'
 PATH_TO_EXCEL = os.path.join(DATA_DIR, "operations.xlsx")
 TRANSACTIONS = get_data_frame_from_excel_file(PATH_TO_EXCEL)
 
-test_result = '''Результат функции:            Дата операции  ... Сумма операции с округлением
-302  12.11.2021 12:44:32  ...                       100.36
-304  12.11.2021 12:34:17  ...                       292.93
-305  11.11.2021 21:09:32  ...                       162.67
-308  11.11.2021 15:25:03  ...                        99.32
-309  10.11.2021 22:04:41  ...                        89.80
-..                   ...  ...                          ...
-819  16.08.2021 13:11:19  ...                        74.50
-822  15.08.2021 12:10:21  ...                       180.97
-823  15.08.2021 12:05:51  ...                       319.99
-824  14.08.2021 18:32:30  ...                       136.97
-825  13.08.2021 17:37:23  ...                        34.00
 
-[181 rows x 15 columns]'''
+def test_spending_by_category(spending_by_category_df):
+    df1 = spending_by_category(TRANSACTIONS, 'Переводы', DATE_TO_TEST)
+    df2 = spending_by_category_df
 
+    df2['Номер карты'] = df2['Номер карты'].astype(df1['Номер карты'].dtype)
 
-def test_get_report_func_result():
-    @get_report_func_result()
-    def my_function(x: int, y: int) -> int:
-        """
-        Сложение двух чисел
-        """
-        return x + y
-
-    result = my_function(1, 6)
-    assert result == 'Результат функции: 7'
-
-
-def test_spending_by_category():
-    assert spending_by_category(TRANSACTIONS, 'Супермаркеты', DATE_TO_TEST) == test_result
+    pd.testing.assert_frame_equal(df1.reset_index(drop=True), df2.reset_index(drop=True))
 
 
 @freeze_time(DATE_TO_TEST)
-def test_spending_by_category_wo_date():
-    assert spending_by_category(TRANSACTIONS, 'Супермаркеты') == test_result
+def test_spending_by_category_wo_date(spending_by_category_df):
+    df1 = spending_by_category(TRANSACTIONS, 'Переводы')
+    df2 = spending_by_category_df
+
+    df2['Номер карты'] = df2['Номер карты'].astype(df1['Номер карты'].dtype)
+
+    pd.testing.assert_frame_equal(df1.reset_index(drop=True), df2.reset_index(drop=True))
